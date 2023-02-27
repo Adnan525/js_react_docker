ea265ca37db2806853f65fa875db40ed42efb3c5
Adnan525 <adnaan525@gmail.com>
2023-02-28 02:25:44 +1100

    variable type and delare

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
-document.write('Hello World from /src/script.js');
+// document.write('Hello World from /src/script.js');
+// console.log('Hello World from /src/script.js');
+
+// can not use let variable outside of block
+var a = "this is a variable using var";
+let b = "this is a variable using let";
+const c = "this is a variable using const";
+
+
+// let won't allow you to redeclare a variable
+    // var myname = "Muntasir";
+    // var myname = "Adnan";
+    // console.log(myname);
+    // let myname = "Muntasir";
+    // let myname = "Adnan";
+    // console.log(myname);
+
+// const won't allow you to redeclare a variable
+// const won't allow you to reassign a variable
+
+//variable types
+//number, string, boolean, null/object, undefined
+var undefinedVariable;
+console.log(typeof(undefinedVariable));
